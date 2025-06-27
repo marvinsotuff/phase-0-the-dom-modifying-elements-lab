64fcd35c362f0f6e7049938068b4a4fb8cb6a97c
Marvin Kioko <kiokomarvin7@gmail.com>
2025-06-27 19:27:20 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code here!
+// Write your code here
+
+// Remove the <main> element with id 'main'
+let mainElement = document.querySelector('main#main');
+
+function removeMainElement() {
+  if (mainElement) {
+    mainElement.remove();
+  }
+}
+removeMainElement();
+
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+
+let YOUR_NAME = 'YOUR_NAME'
+newHeader.innerHTML= `${YOUR_NAME} is the champion`;
